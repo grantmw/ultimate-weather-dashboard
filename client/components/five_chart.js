3ee7a14cd1209b183bf337721f517f597c351831
grantmw <grantmwong@gmail.com>
2016-12-02 16:49:59 -0800

    reusable five day chart component created, plugged into five_day container

---
+++ client/components/five_chart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(numbers) {
+	return _.round(_.sum(numbers)/numbers.length);
+}
+export default (props) => {
+
+	return(
+		<div>
+			<Sparklines height={100} width={140} data={props.data}>
+				<SparklinesLine color={props.color} />
+				<SparklinesReferenceLine type='avg' />
+			</Sparklines>
+			<div>{average(props.data)} {props.units}</div>
+		</div>
+	);
+}
+
--- client/containers/five_day.js
+++ client/containers/five_day.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import Chart from '../components/chart';
+import Chart from '../components/five_chart';
 import GoogleMap from '../components/map.js';
 
-class WeatherList extends Component {
-	
+class FiveDayCharts extends Component {
+	renderFiveDay(cityData) {
+		const name = cityData.city.name;
+		const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => (temp*(9/5))-459.67);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const lat = cityData.city.coord.lat;
+		const lon = cityData.city.coord.lon;
+
+
+		return (
+			<tr key={name}>
+				<td>
+					<GoogleMap lon={lon} lat={lat} />
+				</td>
+				<td>
+					<Chart data={temps} color='red' units='&deg;F' />
+				</td>
+				<td>
+					<Chart data={pressures} color='yellow' units='hPa' />
+				</td>
+				<td>
+					<Chart data={humidities} color='blue' units='%' />
+				</td>
+
+			</tr>
+		);
+	}
+
 }
+
+function mapStateToProps(state) {
+	return {fiveDay: state.fiveDay}
+}
+
+export default connect(mapStateToProps)(FiveDayCharts);
