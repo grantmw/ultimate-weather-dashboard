4280048ad8ab025ac3b28de19127e73163e1573e
grantmw <grantmwong@gmail.com>
2016-12-04 12:47:03 -0800

    data extraction moved to rendercurrent function

--- client/containers/current.js
+++ client/containers/current.js
@@ -3,17 +3,41 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 class Current extends Component {
+
+	renderCurrent() {
+			const icon = this.props.today.weather[0].icon
+			const temp = this.props.today.main.temp
+			const low_temp = this.props.today.main.temp_min
+			const high_temp = this.props.today.main.temp_max
+			const description = this.props.today.weather[0].description
+			const wind = this.props.today.wind.speed
+
+			return(
+				<div>
+					{icon} <br />
+					{temp} <br />
+					{low_temp} <br />
+					{high_temp} <br />
+					{description} <br />
+					{wind} <br />
+				</div>
+			);
+		
+	}
+
+
 	render(){
+
 		console.log(this.props.today);
 		if (!this.props.today) {
 			return (
 				<div>
 				</div>
 			);
-		}
+		} 
 		return(
 			<div className='col-md-8'>
-				{this.props.today.weather[0].icon}
+				{this.renderCurrent()}
 			</div>
 		);
 	}
