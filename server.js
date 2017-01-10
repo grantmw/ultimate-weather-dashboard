933e396c2f488dfacf78702c0633e0232211e4b8
grantmw <grantmwong@gmail.com>
2017-01-10 12:50:54 -0800

    removed server config comments, changed start_dev to startdev for webpack server

--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const port = process.env.PORT || 8080;
-// const app = express();
-
-// app.use(express.static(__dirname));
-
-// // app.get('/api/posts', (req, res) => {
-// //   res.send([{ id: 1 }]);
-// // });
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'index.html'));
-// });
-
-// app.listen(port);
-// console.log('Server started');
-
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
